71d1e61ae82ba9a6221b2a1f3a29048256abd587
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-04-16 21:28:15 -0700

    Implement scanner

---
+++ c/compiler.h
@@ -0,0 +1,6 @@
+#ifndef ghost_compiler_h
+#define ghost_compiler_h
+
+void compile(const char* source);
+
+#endif
--- c/vm.c
+++ c/vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "vm.h"
 #include "debug.h"
 #include <stdio.h>
@@ -113,9 +114,8 @@ static InterpretResult run() {
     #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
+InterpretResult interpret(const char* source) {
+    compile(source);
 
-    return run();
+    return INTERPRET_OK;
 }
