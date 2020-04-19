29f76fbe22f8c661afa88df39ba6c2873bf8a64a
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-04-18 22:04:45 -0700

    Add equality and comparison operators

--- src/compiler.c
+++ src/compiler.c
@@ -150,10 +150,16 @@ static void binary() {
 
     // Emit the operator instruction
     switch (operatorType) {
-        case TOKEN_PLUS:    emitByte(OP_ADD); break;
-        case TOKEN_MINUS:   emitByte(OP_SUBTRACT); break;
-        case TOKEN_STAR:    emitByte(OP_MULTIPLY); break;
-        case TOKEN_SLASH:   emitByte(OP_DIVIDE); break;
+        case TOKEN_BANG_EQUAL:    emitBytes(OP_EQUAL, OP_NOT); break;
+        case TOKEN_EQUAL_EQUAL:   emitByte(OP_EQUAL); break;
+        case TOKEN_GREATER:       emitByte(OP_GREATER); break;
+        case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT); break;
+        case TOKEN_LESS:          emitByte(OP_LESS); break;
+        case TOKEN_LESS_EQUAL:    emitBytes(OP_GREATER, OP_NOT); break;
+        case TOKEN_PLUS:          emitByte(OP_ADD); break;
+        case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
+        case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
+        case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
         default:
             // Unreachable
             return;
@@ -200,46 +206,46 @@ static void unary() {
 }
 
 ParseRule rules[] = {
-    {grouping, NULL, PREC_NONE}, // TOKEN_LEFT_PAREN
-    {NULL, NULL, PREC_NONE},     // TOKEN_RIGHT_PAREN
-    {NULL, NULL, PREC_NONE},     // TOKEN_LEFT_BRACE
-    {NULL, NULL, PREC_NONE},     // TOKEN_RIGHT_BRACE
-    {NULL, NULL, PREC_NONE},     // TOKEN_COMMA
-    {NULL, NULL, PREC_NONE},     // TOKEN_DOT
-    {unary, binary, PREC_TERM},  // TOKEN_MINUS
-    {NULL, binary, PREC_TERM},   // TOKEN_PLUS
-    {NULL, NULL, PREC_NONE},     // TOKEN_SEMICOLON
-    {NULL, binary, PREC_FACTOR}, // TOKEN_SLASH
-    {NULL, binary, PREC_FACTOR}, // TOKEN_STAR
-    {unary, NULL, PREC_NONE},    // TOKEN_BANG
-    {NULL, NULL, PREC_NONE},     // TOKEN_BANG_EQUAL
-    {NULL, NULL, PREC_NONE},     // TOKEN_EQUAL
-    {NULL, NULL, PREC_NONE},     // TOKEN_EQUAL_EQUAL
-    {NULL, NULL, PREC_NONE},     // TOKEN_GREATER
-    {NULL, NULL, PREC_NONE},     // TOKEN_GREATER_EQUAL
-    {NULL, NULL, PREC_NONE},     // TOKEN_LESS
-    {NULL, NULL, PREC_NONE},     // TOKEN_LESS_EQUAL
-    {NULL, NULL, PREC_NONE},     // TOKEN_IDENTIFIER
-    {NULL, NULL, PREC_NONE},     // TOKEN_STRING
-    {number, NULL, PREC_NONE},   // TOKEN_NUMBER
-    {NULL, NULL, PREC_NONE},     // TOKEN_AND
-    {NULL, NULL, PREC_NONE},     // TOKEN_CLASS
-    {NULL, NULL, PREC_NONE},     // TOKEN_ELSE
-    {literal, NULL, PREC_NONE},  // TOKEN_FALSE
-    {NULL, NULL, PREC_NONE},     // TOKEN_FOR
-    {NULL, NULL, PREC_NONE},     // TOKEN_FUNCTION
-    {NULL, NULL, PREC_NONE},     // TOKEN_IF
-    {literal, NULL, PREC_NONE},  // TOKEN_NIL
-    {NULL, NULL, PREC_NONE},     // TOKEN_OR
-    {NULL, NULL, PREC_NONE},     // TOKEN_PRINT
-    {NULL, NULL, PREC_NONE},     // TOKEN_RETURN
-    {NULL, NULL, PREC_NONE},     // TOKEN_SUPER
-    {NULL, NULL, PREC_NONE},     // TOKEN_THIS
-    {literal, NULL, PREC_NONE},  // TOKEN_TRUE
-    {NULL, NULL, PREC_NONE},     // TOKEN_LET
-    {NULL, NULL, PREC_NONE},     // TOKEN_WHILE
-    {NULL, NULL, PREC_NONE},     // TOKEN_ERROR
-    {NULL, NULL, PREC_NONE},     // TOKEN_EOF
+    {grouping, NULL, PREC_NONE},     // TOKEN_LEFT_PAREN
+    {NULL, NULL, PREC_NONE},         // TOKEN_RIGHT_PAREN
+    {NULL, NULL, PREC_NONE},         // TOKEN_LEFT_BRACE
+    {NULL, NULL, PREC_NONE},         // TOKEN_RIGHT_BRACE
+    {NULL, NULL, PREC_NONE},         // TOKEN_COMMA
+    {NULL, NULL, PREC_NONE},         // TOKEN_DOT
+    {unary, binary, PREC_TERM},      // TOKEN_MINUS
+    {NULL, binary, PREC_TERM},       // TOKEN_PLUS
+    {NULL, NULL, PREC_NONE},         // TOKEN_SEMICOLON
+    {NULL, binary, PREC_FACTOR},     // TOKEN_SLASH
+    {NULL, binary, PREC_FACTOR},     // TOKEN_STAR
+    {unary, NULL, PREC_NONE},        // TOKEN_BANG
+    {NULL, binary, PREC_EQUALITY},   // TOKEN_BANG_EQUAL
+    {NULL, NULL, PREC_NONE},         // TOKEN_EQUAL
+    {NULL, binary, PREC_EQUALITY},   // TOKEN_EQUAL_EQUAL
+    {NULL, binary, PREC_COMPARISON}, // TOKEN_GREATER
+    {NULL, binary, PREC_COMPARISON}, // TOKEN_GREATER_EQUAL
+    {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS
+    {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS_EQUAL
+    {NULL, NULL, PREC_NONE},         // TOKEN_IDENTIFIER
+    {NULL, NULL, PREC_NONE},         // TOKEN_STRING
+    {number, NULL, PREC_NONE},       // TOKEN_NUMBER
+    {NULL, NULL, PREC_NONE},         // TOKEN_AND
+    {NULL, NULL, PREC_NONE},         // TOKEN_CLASS
+    {NULL, NULL, PREC_NONE},         // TOKEN_ELSE
+    {literal, NULL, PREC_NONE},      // TOKEN_FALSE
+    {NULL, NULL, PREC_NONE},         // TOKEN_FOR
+    {NULL, NULL, PREC_NONE},         // TOKEN_FUNCTION
+    {NULL, NULL, PREC_NONE},         // TOKEN_IF
+    {literal, NULL, PREC_NONE},      // TOKEN_NIL
+    {NULL, NULL, PREC_NONE},         // TOKEN_OR
+    {NULL, NULL, PREC_NONE},         // TOKEN_PRINT
+    {NULL, NULL, PREC_NONE},         // TOKEN_RETURN
+    {NULL, NULL, PREC_NONE},         // TOKEN_SUPER
+    {NULL, NULL, PREC_NONE},         // TOKEN_THIS
+    {literal, NULL, PREC_NONE},      // TOKEN_TRUE
+    {NULL, NULL, PREC_NONE},         // TOKEN_LET
+    {NULL, NULL, PREC_NONE},         // TOKEN_WHILE
+    {NULL, NULL, PREC_NONE},         // TOKEN_ERROR
+    {NULL, NULL, PREC_NONE},         // TOKEN_EOF
 };
 
 static void parsePrecedence(Precedence precedence) {
