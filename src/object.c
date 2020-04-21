f7125bf8ed78bb9b04c3e04a33da05d3d400758a
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-04-21 11:48:27 -0700

    Work on hash table

--- src/object.c
+++ src/object.c
@@ -19,24 +19,47 @@ static Obj* allocateObject(size_t size, ObjType type) {
     return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
 
     return string;
 }
 
+// The hashString function utilizes the FNV-1a algorithm to
+// create a unique and reproducable hash of the given key
+// with the given length. "FNV" stands for "Fowler/Noll/Vo",
+// named after the creators of the algorithm.
+static uint32_t hashString(const char* key, int length) {
+    // we are implementing a 32bit hash, so the hash and prime
+    // values are set appropriately for this size.
+    uint32_t hash = 2166136261u;
+    uint32_t prime = 16777619u;
+
+    for (int i = 0; i < length; i++) {
+        hash ^= key[i];
+        hash *= prime;
+    }
+
+    return hash;
+}
+
 ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+
+    return allocateString(chars, length, hash);
 }
 
 ObjString* copyString(const char* chars, int length) {
+    uint32_t hash = hashString(chars, length);
+
     char* heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
 
-    return allocateString(heapChars, length);
+    return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
---
+++ src/table.h
@@ -0,0 +1,23 @@
+#ifndef ghost_table_h
+#define ghost_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
+
+#endif
